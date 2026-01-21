0fda43ac2bfbcefa9bbe8143310af765f3ca9e2f
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-01-21 16:32:40 +0530

    learning is in progress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,status,Response
 from pydantic import BaseModel,HttpUrl
 import psycopg2 
 from psycopg2.extras import RealDictCursor
@@ -46,3 +46,50 @@ def studyMart():
 @app.get("/django/api")
 def django():
         return {"type" : "Basic to Advance"}
+
+@app.get("/course/{id}")
+def get_course(id:int):
+       cursor.execute("""SELECT * FROM course where id = %s """, (str(id),))
+       course = cursor.fetchone()
+       if not course:
+              raise HTTPException(
+                     status_code = status.HTTP_404_NOT_FOUND,
+                     details = f"course with id:{id} was not found"
+              )
+       return{"course_details": course}
+
+@app.delete("/course/{id}" , status_code=status.HTTP_204_NO_CONTENT)
+def delete_course(id:int):
+       cursor.execute("""DELETE FROM course where id = %s returning *""", (str(id),))
+       delete_course = cursor.fetchone()
+       conn.commit()
+       if delete_course == None:
+              raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"course with id :{id} does not exist")
+       return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+from fastapi import HTTPException, status, Response
+
+@app.put("/course/{id}")
+def update_course(id: int, course: Course):
+
+    cursor.execute(
+        """ UPDATE course SET name = %s,
+            instructor = %s,
+            duration = %s,
+            website = %s
+        WHERE id = %s
+        RETURNING *;
+        """,
+        (course.name, course.instructor, course.duration, str(course.website), str(id))
+    )
+
+    updated_course = cursor.fetchone()
+    conn.commit()
+
+    if updated_course is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Course with id {id} does not exist"
+        )
+
+    return {"updated_course": updated_course}
