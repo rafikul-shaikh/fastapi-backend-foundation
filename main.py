a22666ff53d866f6466275e9242a4dc2c9b25891
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-01-21 13:52:30 +0530

    learning is in progress

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
 from fastapi import FastAPI
+from pydantic import BaseModel,HttpUrl
+import psycopg2 
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
+# define request body schema
+class Course(BaseModel):
+       name: str
+       instructor: str
+       duration:float
+       website: HttpUrl
+
+while True:
+       try:
+              conn = psycopg2.connect(host = "localhost", port="5433", database ="aiquest", user = "postgres",password = "1234",
+              cursor_factory = RealDictCursor)
+              cursor = conn.cursor()
+              print('successfully connected database')
+              break
+       except Exception as error:
+              print('Databse conection failed')
+              print("Error",error )
+              time.sleep(2)
+
+@app.post("/post")
+def create_post(post:Course):
+       cursor.execute(""" INSERT INTO course(name,instructor, duration,website) VALUES (%s,%s,%s,%s) RETURNING *""", (post.name,post.
+       instructor, post.duration, str(post.website)))
+       new_post = cursor.fetchone()
+       conn.commit()
+       return {"Data": new_post}
+
 @app.get("/")
 def aiquest():
-    return {"Djeango": "API"}
+    cursor.execute(""" SELECT * FROM course """)
+    data = cursor.fetchall()
+    return {"Data": data}
+
+@app.get("/course")
+def studyMart():
+        return {"Course" : "Django and backend developement with python "}
+    
+@app.get("/django/api")
+def django():
+        return {"type" : "Basic to Advance"}
