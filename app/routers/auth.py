79d22d0965d27cc1f6b92b0e3d24b9ac9a232a9b
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-01-29 12:00:25 +0530

    added oauth2.py file and created jwt token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status, HTTPException, Depends, responses
 from sqlalchemy.orm import Session
-from app import database, models, utils, schemas
+from app import database, models, utils, schemas,oauth2
+from datetime import timedelta
 
 router = APIRouter(tags=['Authentication'])
 
@@ -12,4 +13,9 @@ def login(user_credentials :schemas.UserLogin, db:Session=Depends(database.get_d
     if not utils.verify_password(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credential")
     
-    return {"token": 'Successfully login'}
+    access_token = oauth2.create_access_token(
+        data = {"user_id": user.id},
+        expires_delta= timedelta(minutes=oauth2.ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+    
+    return {"access_token": access_token, "token_type":"bearer"}
